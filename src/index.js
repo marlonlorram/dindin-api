73fedda677a6337a4693a4bbcee4e444ef73f473
Marlon Lorram <eiskyenet@gmail.com>
2023-10-22 00:48:22 -0300

    feat: add middleware for standardized error handling

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 
+const errorHandler = require('./middlewares/error.middleware');
+
 const app = express();
 app.use(express.json());
+
+app.use(errorHandler);
 
 const host = process.env.SERVER_HOST || 'localhost';
 const port = process.env.SERVER_PORT || 5000;
---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,27 @@
+/**
+ * @module error.middleware
+ * @author Marlon Lorram (https://github.com/marlonlorram)
+ *
+ * @description Middleware to capture errors that occur during the processing of a request.
+ */
+
+const { HTTP_STATUS } = require('../utils/status_code');
+
+/**
+ * Responds to requests with appropriate information about the occurred error.
+ *
+ * @param {Object} err - Error object.
+ * @param {Express.Request} req - The current request. (Not used directly.)
+ * @param {Express.Response} res - The response to be sent.
+ * @param {Function} next The next middleware. (Not used directly.)
+ */
+const errorHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || HTTP_STATUS.INTERNAL_SERVER_ERROR;
+  const message = err.statusCode
+    ? err.message
+    : `Internal server error: ${err.message}`;
+
+  res.status(statusCode).json({ message });
+};
+
+module.exports = errorHandler;
