5d8d41862e5fc8de5af3c00f887df1aecf5f5c38
Marlon Lorram <eiskyenet@gmail.com>
2023-10-22 13:42:21 -0300

    feat: add login function to auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -7,9 +7,10 @@
 
 const bcrypt = require('bcrypt');
 
-const { badRequest, conflict } = require('../utils/errors');
+const { badRequest, conflict, unauthorized } = require('../utils/errors');
 
 const userRepository = require('../repositories/user.repository');
+const generateToken = require('../helpers/generateToken');
 
 /**
  * Registers a new user.
@@ -42,6 +43,43 @@ const register = async ({ name, email, password }) => {
   return userCreated;
 };
 
+/**
+ * Authenticates a user based on email and password.
+ *
+ * @param {Object} userData - User details.
+ * @param {string} userData.email - User's email.
+ * @param {string} userData.password - User's password.
+ * @return {Object} An object containing the user's details and JWT token.
+ */
+const login = async ({ email, password }) => {
+  if (!email || !password) {
+    throw badRequest('Email and password are required.');
+  }
+
+  const user = await userRepository.findUserByEmail(email);
+
+  if (!user) {
+    throw unauthorized('Invalid email or password.');
+  }
+
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatch) {
+    throw unauthorized('Invalid email or password.');
+  }
+
+  const token = generateToken(user.id);
+
+  const { password: _, ...userWithoutPassword } = user;
+  const userLoggedIn = {
+    user: userWithoutPassword,
+    token,
+  };
+
+  return userLoggedIn;
+};
+
 module.exports = {
   register,
+  login,
 };
