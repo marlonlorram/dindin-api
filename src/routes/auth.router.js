9917b2fc60b56dcfb0222983c26c20f7015feab4
Marlon Lorram <eiskyenet@gmail.com>
2023-11-04 11:22:12 -0300

    docs: add Swagger documentation for authentication endpoints

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -11,7 +11,78 @@ const { register, login } = require('../controllers/auth.controller');
 
 const router = new Router();
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     tags:
+ *       - Auth
+ *     summary: Register a new user
+ *     description: Endpoint to register a user with email and password.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: User's name.
+ *                 example: Your Name
+ *               email:
+ *                 type: string
+ *                 description: User's email.
+ *                 example: example@email.com
+ *               password:
+ *                 type: string
+ *                 description: User's password.
+ *                 example: password123
+ *     responses:
+ *       200:
+ *         description: User registered successfully. Returns an object containing the user's details.
+ *       400:
+ *         description: Bad request due to missing or invalid parameters.
+ *       409:
+ *         description: Conflict due to email already being registered.
+ *       500:
+ *         description: Internal server error.
+ */
 router.post('/register', register);
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     tags:
+ *       - Auth
+ *     summary: Authenticate a user
+ *     description: Endpoint to authenticate a user using their email and password.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: User's email.
+ *                 example: example@email.com
+ *               password:
+ *                 type: string
+ *                 description: User's password.
+ *                 example: password123
+ *     responses:
+ *       200:
+ *         description: User authenticated successfully. Returns an object containing the user's details and JWT token.
+ *       400:
+ *         description: Bad request due to missing or invalid parameters.
+ *       401:
+ *         description: Unauthorized invalid email or password.
+ *       500:
+ *         description: Internal server error.
+ */
 router.post('/login', login);
 
 module.exports = router;
