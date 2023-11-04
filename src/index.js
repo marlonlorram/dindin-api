ce3345fb79216115ddbbf58c1361b5090c3d8752
Marlon Lorram <eiskyenet@gmail.com>
2023-11-04 11:21:38 -0300

    feat: add and configure Swagger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 const authRoutes = require('./routes/auth.router');
 
@@ -9,7 +11,32 @@ const errorHandler = require('./middlewares/error.middleware');
 const app = express();
 app.use(express.json());
 
-app.use('/', authRoutes);
+app.use('/auth', authRoutes);
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Dindin API',
+      description: 'API for personal finance management',
+      version: '1.0.0',
+      license: {
+        name: 'MIT',
+        url: 'https://github.com/marlonlorram/dindin-api/blob/main/LICENSE',
+      },
+    },
+  },
+
+  apis: ['./src/routes/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true }),
+);
 
 app.use(errorHandler);
 
